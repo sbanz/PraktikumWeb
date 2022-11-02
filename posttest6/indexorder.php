2964478d70f8115d2a952929c7ac05be977d7108
sbanz <92090134+sbanz@users.noreply.github.com>
2022-11-02 15:42:40 +0800

    Add files via upload

---
+++ posttest6/indexorder.php
@@ -0,0 +1,26 @@
+<?php
+include 'functions.php';
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>$title</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body>
+    <nav class="navtop">
+    	<div>
+    		<h1>Order</h1>
+            <a href="index.php"><i class="fas fa-home"></i>Home</a>
+    		<a href="read.php"><i class="fa fa-shopping-cart"></i>Order</a>
+    	</div>
+    </nav>
+	<div class="content">
+		<h2>ORDER PAGE</h2>
+		<p>Welcome!</p>
+		<p>You can make your Order at the order bar up top :)</p>
+	</div>
+</html>
